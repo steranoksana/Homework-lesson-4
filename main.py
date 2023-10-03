d779a4519364ddfcfdefc9e0879d88372c2d3697
Oksana Vasyliuk <streranoksana@gmail.com>
2023-10-04 00:01:31 +0300

    Homework lesson 4

---
+++ main.py
@@ -0,0 +1,60 @@
+# string = input("Enter a string: ")
+#
+# char = input("Enter a character to search for: ")
+#
+# count = 0
+#
+# for i in string:
+#
+#       if i == char:
+#
+#        count += 1
+#
+# print(f"The character '{char}' appears {count} times in the string.")
+
+
+# string = input("Enter a string: ")
+#
+# char = input("Enter a character to search for: ")
+#
+# count = 0
+#
+# for c in string:
+#
+#    if c == char:
+#
+#      count += 1
+#
+# print(f"The character '{char}' appears {count} times in the string.")
+#
+# original_string = input("Enter the original string: ")
+#
+# word_to_replace = input("Enter the word to replace: ")
+#
+# replacement_word = input("Enter the replacement word: ")
+#
+# modified_string = original_string.replace(word_to_replace, replacement_word)
+#
+# print("Modified string: " + modified_string)
+
+
+#
+# sentense = "Good day:"
+# print(sentense[2])
+#
+# print(sentense[-3])
+#
+# print(sentense[0:6])
+#
+# print(sentense[: -2])
+#
+# print(sentense[::-1])
+#
+# print(len(sentense))
+#
+# print(len(sentense))
+
+
+
+
+
